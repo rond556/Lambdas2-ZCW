879e43b24d8fe84a73302fdddf1b513bfb1847ce
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-29 10:50:48 -0500

    done

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
@@ -8,13 +9,10 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class MainTest {
+    ArrayList<Person> personList = new ArrayList<>();
 
-    @Rule
-    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-
-    @Test
-    public void localClassTest() {
-        ArrayList<Person> personList = new ArrayList<>();
+    @Before
+    public void setUp() throws Exception {
         Person terra = new Person("Terra Bradford", 18, Person.Sex.FEMALE, "terra@esper.com");
         Person locke = new Person("Locke Cole", 21, Person.Sex.MALE, "locke@treasurehunter.org");
         Person sabin = new Person("Sabin Figaro", 23, Person.Sex.MALE, "sabin@figaro.com");
@@ -23,6 +21,14 @@ public class MainTest {
         personList.add(locke);
         personList.add(sabin);
         personList.add(edgar);
+    }
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+
+    @Test
+    public void localClassTest() {
         class getGender implements CheckPerson {
             @Override
             public boolean test(Person p) {
@@ -35,30 +41,12 @@ public class MainTest {
 
     @Test
     public void lambdaTest(){
-        ArrayList<Person> personList = new ArrayList<>();
-        Person terra = new Person("Terra Bradford", 18, Person.Sex.FEMALE, "terra@esper.com");
-        Person locke = new Person("Locke Cole", 21, Person.Sex.MALE, "locke@treasurehunter.org");
-        Person sabin = new Person("Sabin Figaro", 23, Person.Sex.MALE, "sabin@figaro.com");
-        Person edgar = new Person("Edgar Figaro", 24, Person.Sex.MALE, "edgar@figaro.com");
-        personList.add(terra);
-        personList.add(locke);
-        personList.add(sabin);
-        personList.add(edgar);
         Main.printPersons(personList, (Person p) -> p.getGender().equals(Person.Sex.FEMALE));
         Assert.assertEquals("Person Name: Terra Bradford, Age: 18, Gender: FEMALE, Email Address: terra@esper.com\n", systemOutRule.getLog());
     }
 
     @Test
     public void anonymousClass(){
-        ArrayList<Person> personList = new ArrayList<>();
-        Person terra = new Person("Terra Bradford", 18, Person.Sex.FEMALE, "terra@esper.com");
-        Person locke = new Person("Locke Cole", 21, Person.Sex.MALE, "locke@treasurehunter.org");
-        Person sabin = new Person("Sabin Figaro", 23, Person.Sex.MALE, "sabin@figaro.com");
-        Person edgar = new Person("Edgar Figaro", 24, Person.Sex.MALE, "edgar@figaro.com");
-        personList.add(terra);
-        personList.add(locke);
-        personList.add(sabin);
-        personList.add(edgar);
         Main.printPersons(personList, new CheckPerson() {
             @Override
             public boolean test(Person p) {
